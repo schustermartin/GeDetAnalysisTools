74062c694e9c27087f379bef1b20e67579310f05
Lukas Hauertmann <lhauert@mpp.mpg.de>
2019-09-06 12:51:37 +0200

    remove code duplication in math function definitions

--- src/analysis_tools/0_1_math_functions.jl
+++ src/analysis_tools/0_1_math_functions.jl
@@ -1,90 +1,63 @@
-function gauss(x::T, p::Array{T, 1})::T where {T}
-    scale::T = p[1]
-    σ::T     = p[2]
-    μ::T     = p[3]
-    return @fastmath scale * exp(-0.5 * ((x - μ)^2) / (σ^2)) / (sqrt(2 * π * σ^2))
-end
-
-function gauss(x::Array{T, 1}, p::Array{T, 1})::Array{T, 1} where {T}
-    return T[ gauss(v, p) for v in x ]
-end
-
-function linear_polynom(x::T, p::Array{T, 1})::T where {T}
-	offset::T = p[1]
-	slope::T  = p[2]
-	return @fastmath offset + slope * x
-end
-function linear_polynom(x::Array{T, 1}, p::Array{T, 1})::Array{T, 1} where {T}
-	return T[ linear_polynom(v, p) for v in x ]
-end
-
-function flat_offset(x::T, p::Array{T, 1})::T where {T}
-	offset::T = p[1]
-	return @fastmath offset
-end
-function flat_offset(x::Array{T, 1}, p::Array{T, 1})::Array{T, 1} where {T}
-	return T[ flat_offset(v, p) for v in x ]
+function gauss(x, p)
+    scale = p[1]
+    σ     = p[2]
+    μ     = p[3]
+    return @fastmath @. scale * exp(-0.5 * ((x - μ)^2) / (σ^2)) / (sqrt(2 * π * σ^2))
 end
 
 
-function gauss_plus_first_order_polynom(x::T, p::Array{T, 1})::T where {T}
-    scale::T = p[1]
-    σ::T     = p[2]
-    μ::T     = p[3]
-    cp0::T   = p[4] 
-    cp1::T   = p[5]
+function linear_polynom(x, p)
+	offset = p[1]
+	slope  = p[2]
+	return @fastmath @. offset + slope * x
+end
+
+
+function flat_offset(x, p) 
+	return @. p[1]
+end
+
+function gauss_plus_first_order_polynom(x, p)
+    scale = p[1]
+    σ    = p[2]
+    μ     = p[3]
+    cp0   = p[4] 
+    cp1   = p[5]
     if scale < 0 || σ <= 0 
-        return -T(Inf)
+        return -Inf
     else
-        return @fastmath scale * exp(-0.5 * ((x - μ)^2) / (σ^2)) / (sqrt(2 * π * σ^2)) + cp0 + cp1 * (x - μ)
+        return @fastmath @. scale * exp(-0.5 * ((x - μ)^2) / (σ^2)) / (sqrt(2 * π * σ^2)) + cp0 + cp1 * (x - μ)
     end
 end
 
-function gauss_plus_first_order_polynom(x::Array{T, 1}, p::Array{T, 1})::Array{T, 1} where {T}
-    return T[ gauss_plus_first_order_polynom(v, p) for v in x ]
-end
 
-
-function gauss_plus_fixed_first_order_polynom(x::T, p::Array{T, 1}; offset = 0.0, slope = 0.0)::T where {T}
-    scale::T = p[1]
-    σ::T     = p[2]
-    μ::T     = p[3]
-    cp0::T   = offset
-    cp1::T   = slope
+function gauss_plus_fixed_first_order_polynom(x, p; offset = 0.0, slope = 0.0)
+    scale = p[1]
+    σ     = p[2]
+    μ     = p[3]
+    cp0   = offset
+    cp1   = slope
     if scale < 0 || σ < 0 
         return 0
     else
-        return @fastmath scale * exp(-0.5 * ((x - μ)^2) / (σ^2)) / (sqrt(2 * π * σ^2)) + cp0 + cp1 * (x - μ)
+        return @fastmath @. scale * exp(-0.5 * ((x - μ)^2) / (σ^2)) / (sqrt(2 * π * σ^2)) + cp0 + cp1 * (x - μ)
     end
 end
-function gauss_plus_fixed_first_order_polynom(x::Array{T, 1}, p::Array{T, 1}; offset = 0.0, slope = 0.0)::Array{T, 1} where {T}
-    return T[ gauss_plus_first_order_polynom(v, p, offset = offset, slope = slope) for v in x ]
-end
 
 
-
-
-function exponential_decay(x::T, p::Array{T, 1})::T where {T}
-    return p[1] * exp( -x / p[2])
+function exponential_decay(x, p)
+    return @. p[1] * exp( -x / p[2])
 end    
-function exponential_decay(x::Array{T, 1}, p::Array{T, 1})::Array{T, 1} where {T}
-    return T[ exponential_decay(v, p) for v in x ]
-end
 
-function scaled_cauchy(x::T, p::Array{T, 1})::T where {T}
+
+function scaled_cauchy(x, p)
     scale = p[1]
     σ = p[2]
     μ  = p[3]
-    return scale * σ / (π * (σ^2 + (x - μ)^2))
-end
-function scaled_cauchy(x::Array{T, 1}, p::Array{T, 1})::Array{T, 1} where {T}
-    return T[ scaled_cauchy(v, p) for v in x ]
+    return @. scale * σ / (π * (σ^2 + (x - μ)^2))
 end
 
 
-function linear_function_fixed_offset_at_zero(x::T, p::Array{T, 1})::T where {T}
-    return p[1] * x
-end
-function linear_function_fixed_offset_at_zero(x::Array{T, 1}, p::Array{T, 1})::Array{T, 1} where {T}
-    return T[ linear_function_fixed_offset_at_zero(v, p) for v in x ]
+function linear_function_fixed_offset_at_zero(x, p)
+    return @. p[1] * x
 end
